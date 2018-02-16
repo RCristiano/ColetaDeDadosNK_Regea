79c1db7c56cc995beafad549581ab9e1c01b810f
adao.modesto <adao.modesto@regea.com.br>
2018-02-16 19:10:23 -0200

    Logica para setar a localização

--- app/src/main/java/br/com/regea/coletadedadosnk_regea/PontoFragment.java
+++ app/src/main/java/br/com/regea/coletadedadosnk_regea/PontoFragment.java
@@ -6,13 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PontoFragment extends Fragment {
-
 
     public PontoFragment() {
         // Required empty public constructor
@@ -23,7 +24,17 @@ public class PontoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ponto, container, false);
+        View view = (View) inflater.inflate(R.layout.fragment_ponto, container, false);
+
+        Button btn_gps = (Button) view.findViewById(R.id.btn_gps);
+        btn_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Cadastro)getActivity()).requestLocation(view);
+            }
+        });
+
+        return view;
     }
 
 }
