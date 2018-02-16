b58eb8a4f0708576956e5d391f53278fa2ef2cb0
Rodrigo <rcristianofv@hotmail.com>
2018-02-16 10:36:30 -0200

    Inclusão do layout de cadastro e logica de identificação

--- app/src/main/java/br/com/regea/coletadedadosnk_regea/MainActivity.java
+++ app/src/main/java/br/com/regea/coletadedadosnk_regea/MainActivity.java
@@ -1,7 +1,11 @@
 package br.com.regea.coletadedadosnk_regea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Remover
+        Intent intent = new Intent(this, Cadastro.class);
+        intent.putExtra("CadName", "Nome");
+        startActivity(intent);
+    }
+
+    public void logCad (View view) {
+        EditText cadName = findViewById(R.id.txt_CadName);
+
+        if ( cadName.length() < 3 ) {
+            Toast.makeText(this, "Informe seu nome", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Intent intent = new Intent(this, Cadastro.class);
+        intent.putExtra("CadName", cadName.getText().toString());
+        startActivity(intent);
     }
 }
